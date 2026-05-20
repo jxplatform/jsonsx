2d01e85b91118aae5b35136ffbfa8b871fcf64f5
Kevin Shenk <kevin@avu.nu>
2026-05-20 17:04:34 -0400

    fix(desktop): don't build the msi anywhere but windows!

--- packages/desktop/scripts/build-msi.ts
+++ packages/desktop/scripts/build-msi.ts
@@ -1,3 +1,8 @@
 import { $ } from "bun";
 
+if (process.platform !== "win32") {
+  console.log("[build-msi] Skipping MSI build (not Windows)");
+  process.exit(0);
+}
+
 await $`electrobun-builder build --target wix --update --baseUrl https://github.com/jxsuite/jx/releases/download/`;
